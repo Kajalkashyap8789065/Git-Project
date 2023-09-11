f15a310c376f67a2cb58f0818ea316dcafb80d8e
kajalkashyap878065 <kajalkashyap65912@gmail.com>
2023-09-11 15:10:03 +0530

    add calci

--- calci.java
+++ calci.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
 interface calci{
- int sum();
+ int sum(int a, int b );
 }
  class calciImpl implements calci
 {
-public int add(inta ,int b);
+public int add(int a ,int b);
 { 
 return a+b;
+}
+}
+ class CalciDriver{ 
+public static void main (String []args)
+{
+Scanner sc = new Scanner (System.in);
+int a=sc.nextInt();
+int b = sc.nextInt();
+calciImpl c= new calciImpl();
+System.out.println(a+b+"="+c.sum(a,b));
 }
 }
